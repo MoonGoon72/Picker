a486886459bcae3dc8082e8b60080c221b852e0d
MoonGoon <moongoon.cnu@gmail.com>
2023-09-23 20:49:55 +0900

    Initial Commit

---
+++ Picker/PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerApp.swift
+//  Picker
+//
+//  Created by 문영균 on 2023/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
